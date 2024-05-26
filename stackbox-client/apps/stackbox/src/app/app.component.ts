46cfb9e3e87ef95f3aaca0c8739aac449610f2aa
Drischdaan <drischdaan@gmail.com>
2024-05-26 17:52:05 +0200

    feat(client): adds api library

--- stackbox-client/apps/stackbox/src/app/app.component.ts
+++ stackbox-client/apps/stackbox/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  AppService,
+  GetHealthCheck200Response,
+} from '@stackbox/shared/api-stackbox';
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly appService: AppService = inject(AppService);
+
+  healthCheck$: Observable<GetHealthCheck200Response> =
+    this.appService.getHealthCheck();
+}
--- stackbox-client/apps/stackbox/src/app/app.config.ts
+++ stackbox-client/apps/stackbox/src/app/app.config.ts
@@ -1,10 +1,17 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStackboxApi } from '@stackbox/shared/api-stackbox';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideStackboxApi({ basePath: 'http://localhost:3000' }),
   ],
 };
